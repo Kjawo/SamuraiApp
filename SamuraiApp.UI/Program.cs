e8c453322422f524318189de4c84fb0132fbce18
Konrad Jaworski <konjaworski@gmail.com>
2022-02-12 11:29:19 +0100

    Test bulk operations support

--- SamuraiApp.UI/Program.cs
+++ SamuraiApp.UI/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using Microsoft.EntityFrameworkCore;
 using SamuraiApp.Data;
 using SamuraiApp.Domain;
 
@@ -11,25 +12,46 @@ namespace SamuraiApp.UI
 
         private static void Main(string[] args)
         {
-            _context.Database.EnsureCreated();
-            GetSamurais("Before Add:");
-            AddSamurai();
-            GetSamurais("After Add:");
+            // _context.Database.EnsureCreated();
+            // GetSamurais("Before Add:");
+            // AddSamurai();
+            // GetSamurais("After Add:");
+            AddVariousTypes();
+
             Console.Write("Press any key...");
             Console.ReadKey();
         }
 
-        private static void AddSamurai()
+        private static void AddVariousTypes()
         {
-            var samurai = new Samurai {Name = "Julie"};
-            _context.Samurais.Add(samurai);
+            _context.AddRange(new Samurai { Name = "Shimada" },
+                    new Samurai { Name = "Okamoto" },
+                    new Battle { Name = "Battle of Anegawa" },
+                    new Battle { Name = "Battle of Nagashino" });
+
             _context.SaveChanges();
         }
 
-        private static void GetSamurais(string text)
+        private static void AddSamuraisByName(params string[] names)
         {
-            var samurais = _context.Samurais.ToList();
-            Console.WriteLine($"{text}: Samurai count is {samurais.Count}");
+            foreach (string name in names)
+            {
+                _context.Samurais.Add(new Samurai { Name = name });
+            }
+            _context.SaveChanges();
+        }
+        private static void AddSamurais(Samurai[] samurais)
+        {
+            //AddRange can take an array or an IEnumerable e.g. List<Samurai>
+            _context.Samurais.AddRange(samurais);
+            _context.SaveChanges();
+        }
+        private static void GetSamurais()
+        {
+            var samurais = _context.Samurais
+                .TagWith("ConsoleApp.Program.GetSamurais method")
+                .ToList();
+            Console.WriteLine($"Samurai count is {samurais.Count}");
             foreach (var samurai in samurais)
             {
                 Console.WriteLine(samurai.Name);
