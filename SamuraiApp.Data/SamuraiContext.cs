0f883b3cb2991b84d30444d2b9af49cb82448f9b
Konrad Jaworski <konjaworski@gmail.com>
2022-02-26 12:17:28 +0100

    Use SamuraiContext in the simple Samurai API

--- SamuraiApp.Data/SamuraiContext.cs
+++ SamuraiApp.Data/SamuraiContext.cs
@@ -7,6 +7,15 @@ namespace SamuraiApp.Data
 {
     public class SamuraiContext : DbContext
     {
+        public SamuraiContext()
+        {
+
+        }
+        public SamuraiContext(DbContextOptions<SamuraiContext> options) 
+            :base(options)
+        {
+
+        }
         public DbSet<Samurai> Samurais { get; set; }
         public DbSet<Quote> Quotes { get; set; }
         public DbSet<Battle> Battles { get; set; }
